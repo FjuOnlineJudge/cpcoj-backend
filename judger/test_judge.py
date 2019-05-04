6a58d20e62196f898aa4101e0e09f48f87703001
roy4801 <a82611141@gmail.com>
2019-05-04 17:45:19 +0800

    [back] change test py

--- judger/test_judge.py
+++ judger/test_judge.py
@@ -5,14 +5,52 @@ from judge import *
 from utils import dump
 import json
 
-code = ''
-with open('../testProb/1.cpp', 'r') as f:
-	code = ''.join(f.readlines())
+code = r"""
+#include <bits/stdc++.h>
+using namespace std;
 
+typedef unsigned long long ULL;
 
-# tmp = []
-# for _ in range(10):
-# 	tmp.append(Judger())
+#define AC
+
+#ifdef TLE
+ULL fibo(int n)
+{
+	if(n == 0 || n == 1) return n;
+	else
+		return fibo(n-1) + fibo(n-2);
+}
+#endif
+
+#ifdef AC
+#define N 1000000
+ULL a[N+5];
+ULL fibo2(int n)
+{
+	if(n == 0 || n == 1) return n;
+	else if(!a[n])
+		return (a[n] = fibo2(n-1) + fibo2(n-2));
+	else
+		return a[n];
+}
+#endif
+
+int n;
+int main()
+{
+	#ifdef AC
+	while(cin >> n)
+		cout << fibo2(n) << '\n';
+	#endif
+
+	#ifdef TLE
+	while(cin >> n)
+		cout << fibo(n) << '\n';
+	#endif
+	return 0;
+}
+"""
+
 a = Judger()
 
 CASE = 4
